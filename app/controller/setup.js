250f9ef77d9582f8c8569cf224547a0f380cbac5
柴荣臻 <chairongzhen@chairongzhendeMacBook-Pro.local>
2019-02-20 11:00:27 +0800

    update version interface

--- app/controller/setup.js
+++ app/controller/setup.js
@@ -117,7 +117,7 @@ class SetupController extends Controller {
     async checkversion() {
         const ctx = this.ctx;
         let res = await ctx.service.setup.checkversion();
-        ctx.body = res[0]["version"];
+        ctx.body = "version:" + res[0]["version"];
     }
 }
 
