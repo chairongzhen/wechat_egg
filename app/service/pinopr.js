7ad6a29c5f27ca1ff15cdd3a7ba5fc33b6a89ee2
柴荣臻 <chairongzhen@chairongzhendeMacBook-Pro.local>
2019-07-10 17:20:00 +0800

    debug

--- app/service/pinopr.js
+++ app/service/pinopr.js
@@ -291,7 +291,7 @@ class PinoprSerive extends Service {
         // }
         // let result = this.app.mysql.query(updstr).affectedRows == 0 ? false : true;
         // return result;
-        await this.ctx.app.mqttclient.publish("esp_24:0A:C4:9F:85:5C/p", "hello world", { qos: 2 });
+        await this.ctx.app.mqttclient.publish("esp_24:0A:C4:9F:85:5C/setp", "hello world", { qos: 2 });
         return true;
     }
 
