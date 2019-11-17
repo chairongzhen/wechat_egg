e58f7f4474f54c28783658c38aca160030f2c20d
Eric.Chai <241498527@qq.com>
2019-11-17 11:04:53 +0800

    mqtt

--- app/service/pinopr.js
+++ app/service/pinopr.js
@@ -389,7 +389,7 @@ class PinoprSerive extends Service {
             let sender = ta + "/setp";
             //await this.ctx.app.mqttclient.publish(sender, content, { qos: 2 });
             //await this.ctx.app.mqttclient.publish(sender, content, { qos: 2 });
-            await this.ctx.app.mqttclient.publish(sender,"[100,100,100,100,100,100,100,100]",{ qos : 2});
+            await this.ctx.app.mqttclient.publish(sender,"{144:[100,100,100,100,100,100,100,100]}",{ qos : 2});
         }
         let result = this.app.mysql.query(updstr).affectedRows == 0 ? false : true;
         return result;
@@ -550,7 +550,7 @@ class PinoprSerive extends Service {
         for (let ta of onlinemac) {
             let sender = ta + "/setp";
             //await this.ctx.app.mqttclient.publish(sender, content, { qos: 2 });
-            await this.ctx.app.mqttclient.publish(sender,"[{1: [10,10,10,10,10,10,10,10]},{1: [10,10,10,10,10,10,10,10]},{1: [10,10,10,10,10,10,10,10]},{1: [10,10,10,10,10,10,10,10]},{1: [10,10,10,10,10,10,10,10]},{1: [10,10,10,10,10,10,10,10]},{1: [10,10,10,10,10,10,10,10]},{1: [10,10,10,10,10,10,10,10]}]",{ qos: 2})
+            await this.ctx.app.mqttclient.publish(sender,"[{1: [10,10,10,10,10,10,10,10]}]",{ qos: 2})
         }
         console.log("here we go test setp");
         return true;
