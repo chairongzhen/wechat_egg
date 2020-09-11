6dcfc0af22d24b686cd9bef184f267635bed4de1
Eric.Chai <241498527@qq.com>
2020-09-11 21:44:26 +0800

    update

---
+++ jinrong/precache-manifest.346d640f0a6450719c4d47ecbda3e283.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecab6f519d40958abe65",
+    "url": "./static/css/main.b9520e01.chunk.css"
+  },
+  {
+    "revision": "ecab6f519d40958abe65",
+    "url": "./static/js/main.ecab6f51.chunk.js"
+  },
+  {
+    "revision": "58738e1d7589f697e62b",
+    "url": "./static/css/1.3b8f6406.chunk.css"
+  },
+  {
+    "revision": "58738e1d7589f697e62b",
+    "url": "./static/js/1.58738e1d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "70bb64bc252702a8bf0db00fc1d455ee",
+    "url": "./static/media/center-content.70bb64bc.svg"
+  },
+  {
+    "revision": "c0434ff78a790cb151c11d841089aa9b",
+    "url": "./static/media/popular_select.c0434ff7.svg"
+  },
+  {
+    "revision": "0b53072657336ba951bd73b67a728465",
+    "url": "./static/media/recommend_select.0b530726.svg"
+  },
+  {
+    "revision": "04559e9f89df13d4068ea5d9c8f9e649",
+    "url": "./static/media/hearder.04559e9f.svg"
+  },
+  {
+    "revision": "905fe6d197a39ce96862e1c20bf5ade5",
+    "url": "./static/media/popular.905fe6d1.svg"
+  },
+  {
+    "revision": "8449df7e929248af3a9d227ccc123be8",
+    "url": "./static/media/background.8449df7e.jpg"
+  },
+  {
+    "revision": "8248d0065c432caf5d5e90f12b3b68e7",
+    "url": "./static/media/recommend.8248d006.svg"
+  },
+  {
+    "revision": "854bfadb94e9e1d763aa58d7cf2e0fea",
+    "url": "./static/media/titleright.854bfadb.svg"
+  },
+  {
+    "revision": "673b40e0155e24b958ca6090c544b1b2",
+    "url": "./static/media/titleleft.673b40e0.svg"
+  },
+  {
+    "revision": "cba454324a0c5b6a2cdaebd79f1895fc",
+    "url": "./index.html"
+  }
+];
