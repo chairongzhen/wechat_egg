aef751f4191199d63fe5036e93cf11b9f2d14078
Eric.Chai <241498527@qq.com>
2021-01-15 10:20:58 +0800

    update

--- app/controller/account.js
+++ app/controller/account.js
@@ -138,7 +138,11 @@ class AccountController extends Controller {
         let username = ctx.request.body.username;
         let pwd = ctx.request.body.pwd;
         const result = await this.service.account.login(username,pwd);
-        this.ctx.cookies.set("username",'hellocookie')
+        this.ctx.cookies.set("username",'hellocookie',{
+            maxAge: 1000 * 3600 * 24,
+            sameSite: false,
+            secure: true
+        })
         this.ctx.body = result;
     }
 
