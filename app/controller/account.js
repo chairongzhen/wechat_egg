500ddbb217c53f4cad63b254d82296ec836977af
ERIC <241498527@qq.com>
2021-07-19 15:31:20 +0800

    update

--- app/controller/account.js
+++ app/controller/account.js
@@ -173,8 +173,8 @@ class AccountController extends Controller {
 
         let openid = ctx.request.body.openid;
         let mid = ctx.request.body.mid;
-        var reg = /^esp_[A-Z,a-z,0-9]{12}$/;
-        //if(reg.test(mid)) {
+        var reg = /^esp|eqs120_[A-Z,a-z,0-9]{12}$/;
+        if(reg.test(mid)) {
         if(true){
             const resultres = await this.service.account.bindmid(openid,mid);
             if(resultres) {
